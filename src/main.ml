69ee473142431947ad42b7f6968d3679999a6a2f
Michael Terrell <cwithmichael@gmail.com>
2025-03-21 00:04:48 -0500

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,27 @@
 let ( let* ) = Lwt.bind
 
+let handle_client (input, output) =
+  let rec handle_command () =
+    let buf = Bytes.make 1024 '0' in
+    let* n = Lwt_io.read_into input buf 0 1024 in
+    match n with
+    | 0 -> Lwt_io.printl "Client disconnected"
+    | size ->
+        let* () =
+          Lwt_io.printl ("Received: " ^ String.sub (Bytes.to_string buf) 0 size)
+        in
+        let* () = Lwt_io.write output "+PONG\r\n" in
+        handle_command ()
+  in
+  handle_command ()
+
+let rec accept_connections server_socket =
+  let* client_socket, _addr = Lwt_unix.accept server_socket in
+  let input = Lwt_io.of_fd ~mode:Lwt_io.input client_socket in
+  let output = Lwt_io.of_fd ~mode:Lwt_io.output client_socket in
+  Lwt.async (fun () -> handle_client (input, output));
+  accept_connections server_socket
+
 let start_server port =
   let sockaddr = Unix.(ADDR_INET (inet_addr_any, port)) in
   let server_socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
@@ -8,8 +30,7 @@ let start_server port =
   let* () = Lwt_unix.bind server_socket sockaddr in
   Lwt_unix.listen server_socket 10;
   let* () = Lwt_io.printlf "Server started on port %d" port in
-  let* _sock, _addr = Lwt_unix.accept server_socket in
-  Lwt.return_unit
+  accept_connections server_socket
 
 let () =
   let port = 6379 in
